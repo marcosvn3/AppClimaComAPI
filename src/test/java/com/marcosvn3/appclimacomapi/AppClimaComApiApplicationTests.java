ce11a4bf57d02c262b8a5b9ecd79fa084e076810
Marcos santos <marcosvn3@academico.ufs.br>
2025-08-11 21:03:03 -0300

    Initial commit

---
+++ src/test/java/com/marcosvn3/appclimacomapi/AppClimaComApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosvn3.appclimacomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppClimaComApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
